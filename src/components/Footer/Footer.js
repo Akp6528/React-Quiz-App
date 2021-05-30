f31210c409d768e4b015a3200a2da48da4a58f5d
Akp6528 <pandeyashwani999@gmail.com>
2021-05-30 12:48:29 +0530

    final commit

---
+++ src/components/Footer/Footer.js
@@ -0,0 +1,20 @@
+const Footer = () => {
+  return (
+    <div
+      style={{
+        textAlign: "center",
+        marginBottom: 10,
+      }}
+    >
+      Made with ♥ by{" "}
+      <a
+        href="https://www.linkedin.com/in/ashwani-pandey-b96344197/"
+        style={{ cursor: "pointer" }}
+      >
+        Ashwani Pandey
+      </a>
+    </div>
+  )
+}
+
+export default Footer
